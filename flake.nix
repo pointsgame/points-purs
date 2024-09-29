51d23c96de35a1f159e9745229c143be435a2140
Evgeny Kurnevsky <kurnevsky@gmail.com>
2024-09-29 16:02:57 +0200

    Add flake.

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+    };
+
+    purescript-overlay = {
+      type = "github";
+      owner = "thomashoneyman";
+      repo = "purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.purescript-overlay.overlays.default ];
+        };
+      in {
+        devShell = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            nodePackages.purescript-language-server
+            nodePackages.purs-tidy
+            purescript
+            spago-unstable
+            purs-backend-es
+            purescm
+            chez
+            pcre2
+          ];
+
+          LD_LIBRARY_PATH = inputs.nixpkgs.lib.makeLibraryPath buildInputs;
+        };
+      });
+}
